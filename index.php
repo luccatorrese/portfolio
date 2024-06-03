ab2d6c92167be9cc7738e89e6710c50c5206068e
Lucca Torrese <123151223+luccatorrese@users.noreply.github.com>
2024-06-03 16:04:16 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-﻿<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
+
+    <!-- page principale du portfolio -->
+
 <head>
+    <link rel="stylesheet" href="index.css" />
+    <link rel="icon" href="views/images/home.png" alt="Icône de la page d'accueil" />
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Portfolio</title>
-    <!-- utilisation de la police Inter via le serveur Google. utilisée également sur Figma. -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Montserrat&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Mon portfolio</title>
 </head>
+
 <body>
+    <main>
 		<?php
 		// connexion à la base de données afin d'obtenir l'instance PDO
 		$pdo = require './modele/connect.php';
@@ -30,72 +37,56 @@
         <!-- Sections de la bannière apparaissant dans la table nav_bar -->
         <nav>
             <ul>
-            <?php
-                foreach($data_nav as $item){
-                    if($item['show']==1){
-                        echo "<li><a href=".$item['link'].">".$item['text']."</a> </li>";
-                    }
-                }
-            ?>
+                <li><a class="liennav" href="/Portfolio/views/interests.php">Mes centres d'intérêt</a> </li>
+                <li><a class="liennav" href="/Portfolio/views/skills.php">Mes compétences</a> </li>
+                <li><a class="liennav" href="/Portfolio/views/contact.php">Contact</a> </li>
             </ul>
         </nav>
     </header>
 
-    <main>
     <!-- Portrait personnel et paragraphe de présentation. -->
-        <?php
-            if ($data_nav[0]['show']==1)
-            {
-        ?>
         <div class="welcome">
-            <div class="welcome-imageside">
-                <div class="fausseimg">
-                    <img src="images/Portrait.jpg" alt="" />
-                </div>
-            </div>
             <div class="welcome-textside">
-                <h1>Bienvenue sur mon site !</h1>
+                <h1>Bienvenue sur mon portfolio !</h1>
                 <p>
-                    Pour me présenter : je m’appelle Lucca Torrese, j’ai 18 ans, et je suis actuellement en première année de BUT MMI.
-                    La création de ce site web rentre également dans mes projets de formation à réaliser. Ma signature, créée avec Adobe
-                    Illustrator, est présente en fin de page pour que l’on puisse mieux m’identifier à l’avenir.
-                    Ce projet de site Web parle principalement de mes centres d’intérêt recensés juste en-dessous de ma propre photo,
-                    ainsi que de mes capacités et de mes compétences, disponible dans le menu A propos, afin que vous puissiez mieux me connaître.
-                    Un court menu pour me contacter est également disponible en fin de page.
+                    Je m’appelle Lucca Torrese, j’ai 19 ans, je suis né à Bastia, et je suis actuellement en deuxième année de BUT MMI. La formation dure trois ans. La création de ce portfolio rentre dans mes projets de formation à réaliser en MMI, sous la forme d'une Situation d'Apprentissage et d'Evaluation, dénommée SAE par nos professeurs.
+                </p>
+                <p>
+                    Je te donne un aperçu de ma signature en fin de présentation, créée avec Adobe Illustrator et Adobe Photoshop durant ma première année de MMI. Dans ce projet, chaque étudiant avait pour objectif de créer sa propre signature, au format papier ou au format numérique, afin de donner une nouvelle représentation graphique à sa propre identité.
+                </p>
+                <p>
+                    Ce portfolio parle notamment de mes compétences et de mes centres d'intérêt recensés dans les différents menus que tu trouveras dans l'en-tête, afin que tu puisses mieux me connaître.
                 </p>
             </div>
+            <div class="welcome-imageside">
+                <div class="fausseimg">
+                    <img src="views/images/Portrait.jpg" alt="Mon portrait" />
+                </div>
+            </div>
         </div>
-        <?php
-            };
-        ?>  
- 
+
+        <div class="signature">
+            <h1>Signature</h1>
+            <div class="photo-signature">
+                <img src="views/images/signature.jpg" alt="Signature officielle" />
+            </div>
+        </div>
+
         <?php
             if ($data_nav[1]['show']==1)
             {
         ?>
-        <div class="interests">
-            <h1>Mes centres d’intérêt</h1>
-            <p>
-                J’aime tout ce qui est jeux vidéo, cuisine, informatique, jouer aux échecs
-                ou encore dessiner. C’est ce qui remplit principalement mon temps libre.
-                Lorsque je ne travaille pas ou que je n’ai pas la tête à travailler, je me
-                consacre généralement à un de mes centres d’intérêt pour passer le temps.
-                Mes centres d'intérêts me permettent d’apprendre beaucoup de choses, et j’estime
-                que chaque domaine a ses spécialités. De plus, l’informatique est riche en fonctionnalités
-                et on peut donc en apprendre beaucoup chaque jour. C'est pourquoi j'en ai fait ma
-                formation : elle propose un tas d’activités diversifiées mais également de faire de
-                la programmation et de visiter un peu plus l’univers de l’informatique, ce qui
-                paraissait intéressant pour moi.
-            </p>
-            <div class="img-interets">
-                <img src="images/Skaknew_echiquier_depart.png" class="echecs" alt="Echiquier" />
-                <br/>
-                <span><i>Les échecs font partie des jeux que j'apprécie.</i></span>
-                <img src="images/playstation_4.png" class="ps" alt="PlayStation 4" />
-                <br/>
-                <span><i>La PlayStation me permet parfois d'occuper mon temps libre.</i></span>
-            </div>
+        
+        <!--
+        <div class="bienvenue">
+            <p>Notes pour le développeur :</p>
+                <p>- ajouter des animations.</p>
+                <p>- ajouter des boutons.</p>
+                <p>- ajouter un système d'envoi de mail via formulaire de contact.</p>
+                <p>- ajouter les projets en cours (exemples : bière Roccapina, projet de jeu vidéo ...)</p>
         </div>
+        -->
+
         <?php
             };
         ?>  
@@ -104,23 +95,6 @@
             if ($data_nav[2]['show']==1)
             {
         ?>
-        <div class="about">
-            <h1>Mes compétences</h1>
-            <p>
-                Je suis généralement doué pour coder certains programmes simples en langage HTML, cependant lorsque
-                je me retrouve en difficulté je n’hésite pas à me renseigner auprès d’autres sites Internet ou de mes profs,
-                en particulier pour les projets pédagogiques. Selon moi, le codage parait intéressant car il permet non seulement de créer des
-                sites Web comme je fais en ce moment même, mais aussi d’inventer des programmes qui pourraient
-                peut-être, à un haut niveau, servir dans la vie de tous les jours.
-                Je possède également de bonnes capacités ainsi qu’une bonne culture dans le domaine
-                cinématographique. Etant plutôt cinéphile, j’ai tendance à bien jouer mon rôle devant la caméra, à condition bien sûr que je
-                maîtrise mon texte correctement. La formation propose régulièrement aux étudiants d’écrire des scénarios, ce que j’arrive à faire
-                convenablement, selon mon imagination.
-            </p>
-            <img src="images/spider-man.png" class="spiderman" alt="" />
-            <br/>
-            <span><i>Spider-Man, qui fut beaucoup représenté au cinéma, fait partie des super-héros que j'apprécie particulièrement.</i></span>
-        </div>
         <?php
             };
         ?>  
@@ -129,72 +103,6 @@
             if ($data_nav[3]['show']==1)
             {
         ?>
-        <div class="contact">
-            <h1>Contact</h1>
-            <p>
-                Pour me contacter, vous pouvez simplement me joindre par mon mail personnel, ou par Discord. Si vous décidez de me contacter par Discord,
-                il suffira de m’envoyer une invitation. A noter que je préfère la boîte personnelle plutôt que la boîte universitaire, car c’est en général
-                là où je reçois les mails les plus importants. Mon temps de réponse est généralement assez rapide, voire immédiate : cela dépend
-                de l'heure, ainsi que des mails que je reçois. On dit souvent que les réponses rapides
-                sont bien vues en entreprise, donc cela représente un avantage !
-            </p>
-
-            <!-- fin de page avec la signature et les icônes -->
-
-            <img class="signature" src="images/signature.jpg" alt="" />
-            <span><i>Ma signature, créée et imaginée grâce à Adobe Illustrator et Adobe Photoshop.</i></span>
-            <br />
-            <p>Pour me contacter via les réseaux sociaux :</p>
-            <img class="facebook" src="images/icone_facebook.png" alt="" />
-            <img class="instagram" src="images/Instagram_icon.png" alt="" />
-            <img class="youtube" src="images/icone_youtube.png" alt="" />
-
-            <form name="Formulaire de contact" action="index.php" method="post">
-                <p>Pour me contacter directement :</p>
-                <table class="form-style">
-                    <tr>
-                        <td>
-                            <label>
-                                Votre nom <span class="required">*</span>
-                            </label>
-                        </td>
-                        <td>
-                            <input type="text" name="name" class="long" />
-                            <span class="error" id="errorname"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>
-                                Votre adresse e-mail <span class="required">*</span>
-                            </label>
-                        </td>
-                        <td>
-                            <input type="email" name="email" class="long" />
-                            <span class="error" id="erroremail"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>
-                                Votre message <span class="required">*</span>
-                            </label>
-                        </td>
-                        <td>
-                            <textarea name="message" class="long field-textarea"></textarea>
-                            <span class="error" id="errormsg"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <input type="submit" name="envoi" value="Envoi" class="b-envoi" />
-                            <input type="reset" value="Réinitialiser" class="b-reset" />
-                        </td>
-                    </tr>
-                </table>
-            </form>
-        </div>
         <?php
             };
         ?>
@@ -204,8 +112,7 @@
         <!-- Bannière de fin avec les mentions légales et le mois de finalisation du portfolio -->
         <nav>
             <ul>
-                <li><a class="liennav" href="#mentions">Mentions légales</a></li>
-                <li><a class="liennav" href="#date">Avril 2023</a></li>
+                <li><a class="liennav">© 2024 IUT de Corse. Tous droits réservés.</a></li>
             </ul>
         </nav>
     </footer>
---
+++ views/skills.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="style-skills.css" />
+        <title>Mes compétences</title>
+        <link rel="icon" href="images/home.png" alt="Icône de la page d'accueil" />
+    </head>
+
+    <header>
+        <!-- Sections de la bannière apparaissant dans la table nav_bar -->
+        <nav>
+            <ul>
+                <li><a class="liennav" href="/Portfolio/index.php">Menu principal</a> </li>
+                <li><a class="liennav" href="/Portfolio/views/interests.php">Mes centres d'intérêt</a> </li>
+                <li><a class="liennav" href="/Portfolio/views/contact.php">Contact</a> </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+    <div class="skills">
+        <h1>Mon expérience</h1>
+        <div class="skills-textside">
+            <p>
+                Je suis généralement doué pour coder certains programmes simples en langage HTML, cependant lorsque
+                je me retrouve en difficulté je n’hésite pas à me renseigner auprès d’autres sites Internet, de mes profs,
+                ou encore de mon entourage, en particulier pour les projets pédagogiques. Selon moi, le codage parait intéressant
+                car il permet non seulement de créer des
+                sites Web comme je fais en ce moment même, mais aussi d’inventer des programmes qui pourraient
+                peut-être, à un haut niveau, servir dans la vie de tous les jours.
+            </p>
+            <p>
+                Je possède de bonnes capacités ainsi qu’une bonne culture dans le domaine
+                cinématographique. Etant cinéphile, j’ai tendance à bien jouer mon rôle devant la caméra, à condition que je
+                maîtrise mon texte convenablement. La formation propose régulièrement aux étudiants d’écrire des scénarios, ce que j’arrive à faire
+                convenablement, selon mon imagination.
+            </p>
+            <p>
+                En avril 2023, j'ai réalisé pour la première fois des photos de tournage pour un projet de MMI
+                qui ont servi à découvrir les acteurs, les décors ainsi que l'équipe technique ayant participé à la réalisation de ce tournage.
+                La présentation des photos de tournage pour ma classe fut pour moi un succès, car celles-ci furent particulièrement appréciées de mon public,
+                qui ont fait remarquer le soin et l'attention apportée à la plupart des photos qui ont été prises.
+            </p>
+        </div>
+            <!-- début compétences MMI -->
+            <div class="img-skills">
+            <h1>Mes compétences</h1>
+            <ul>
+                <li>
+                    <img src="images/CSS-3.png" alt="Logo CSS-3" />
+                    <p>60%</p>
+                    <img src="images/HTML5_logo_and_wordmark.svg.png" alt="Logo HTML-5" />
+                    <p>75%</p>
+                    <img src="images/PHP-logo.svg.png" alt="Logo PHP" />
+                    <p>25%</p>
+                    <img src="images/JavaScript-logo.png" alt="Logo JavaScript" />
+                    <p>25%</p>
+                    <img src="images/Adobe_Illustrator_CC_icon.svg.png" alt="Logo Adobe Illustrator" />
+                    <p>60%</p>
+                    <img src="images/Adobe_Photoshop_CC_icon.svg.png" alt="Logo Adobe Photoshop" />
+                    <p>60%</p>
+                </li>
+            </ul>
+            </div>
+            <!-- fin compétences MMI -->
+    </div>
+    </main>
+
+    <footer>
+        <!-- Bannière de fin -->
+        <nav>
+            <ul>
+                <li><a class="liennav" href="#date">© 2024 IUT de Corse. Tous droits réservés.</a></li>
+            </ul>
+        </nav>
+    </footer>
+
+</html>
